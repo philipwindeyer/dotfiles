6470e07038a397500650b77e4c54bc4ee00e6d55
Philip Windeyer <philipwindeyer@gmail.com>
2024-01-06 17:57:20 +1100

    add *nix bins from git bash installation to Powershell path

--- windows/powershell/config.ps1
+++ windows/powershell/config.ps1
@@ -3,3 +3,7 @@
 # Note: this is sourced/invoked from $PROFILE
 
 . $PSScriptRoot\aliases.ps1
+
+$Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\usr\bin"
+$Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\bin"
+$Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\cmd"
