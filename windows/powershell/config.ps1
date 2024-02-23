2357c6384955bcfdb238a9a350d80c5a54c87fc1
Philip Windeyer <philipwindeyer@gmail.com>
2024-02-23 15:53:51 +1100

    remove PERSONAL_SERVER vars from pws config

--- windows/powershell/config.ps1
+++ windows/powershell/config.ps1
@@ -7,6 +7,3 @@
 $Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\usr\bin"
 $Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\bin"
 $Env:Path += [IO.Path]::PathSeparator + "C:\Program Files\Git\cmd"
-
-$Env:PERSONAL_SERVER = 'Add `$Env:PERSONAL_SERVER = "<username>@<your-personal-server-address-here-if-you-have-one>"` to your powershell profile'
-$Env:PERSONAL_SERVER_KEY = 'Add `$Env:PERSONAL_SERVER_KEY = "/path/to/your/personal/server/key/file"` to your powershell profile'
