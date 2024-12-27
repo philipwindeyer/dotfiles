c60543f175fd4fe3ae83e62a355110325615a2e5
Philip Windeyer <philipwindeyer@gmail.com>
2024-12-27 12:58:22 +1100

    add airclap again, and remove rogue echo

--- macos/lib/setup-fns.sh
+++ macos/lib/setup-fns.sh
@@ -191,7 +191,6 @@ function set_dock_apps() {
   log_heading "Setting Dock Apps"
 
   while IFS='' read -r LINE || [ -n "${LINE}" ]; do
-    echo ${LINE}
     defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>$(eval echo "${LINE}")</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
   done <$1
 
