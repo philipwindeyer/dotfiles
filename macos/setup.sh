7a39de3753b70520ce2d81e10a914cc3c4652dbb
Philip Windeyer <philip.windeyer@qantashotels.com>
2025-08-01 12:05:30 +1000

    add TODO for sudo touch id

--- macos/setup.sh
+++ macos/setup.sh
@@ -40,6 +40,11 @@ npm install -g corepack@latest
 
 # TODO: Install rvm or rbenv for ruby versions
 
+# TODO: enable sudo Touch ID
+# sudo cp /etc/pam.d/sudo_local.template /etc/pam.d/sudo_local
+# Locate the line "auth       sufficient     pam_tid.so" (minus the spaces), and
+# Uncomment it (can probably sed it)
+
 configure_git
 create_directories
 
